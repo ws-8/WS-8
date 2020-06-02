9bcfbe4d6a5f065b4ce1d671fe84c3fac0d6345c
iamotter <uemura@mois.asia>
2020-06-02 17:12:18 +0900

    バグってたmifrationfileの修正

--- db/migrate/20200525043319_create_articles.rb
+++ db/migrate/20200525043319_create_articles.rb
@@ -1,8 +1,9 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.binary :cryped_pass
-      t.timestamp
+class CreateArticles < ActiveRecord::Migration[5.1]                                                                      def change$
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
   end
 end
