084cae067e6696b9ce3759510d62e1648e0fd83f
iamotter <uemura@mois.asia>
2020-06-02 17:25:30 +0900

    文字コードエラー修正,

--- db/migrate/20200525043319_create_articles.rb
+++ db/migrate/20200525043319_create_articles.rb
@@ -1,4 +1,5 @@
-class CreateArticles < ActiveRecord::Migration[5.1]                                                                      def change$
+class CreateArticles < ActiveRecord::Migration[5.1] 
+  def change
     create_table :articles do |t|
       t.string :title
       t.text :content
