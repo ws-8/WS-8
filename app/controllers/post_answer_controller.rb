b249ad4f2e2b326d92882a09c7fae8b0f0d490dc
Ayato Watanabe <38004439+WhiteFox-Lugh@users.noreply.github.com>
2020-07-02 17:16:48 +0900

    feat :new: : dynamic lecture menu

--- app/controllers/post_answer_controller.rb
+++ app/controllers/post_answer_controller.rb
@@ -1,4 +1,7 @@
 class PostAnswerController < ApplicationController
   def new
+    @courses = Course.select("course_name")
+    query = 'SELECT course_name, title FROM lessons, courses WHERE courses.id == lessons.classid'
+    @lessons = Course.find_by_sql(query)
   end
 end
