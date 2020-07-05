a19bffc6a47d31a7856b3c3b281713c256cccd4b
nakanishi <nigiiro7716@gmail.com>
2020-07-05 12:10:51 +0900

    modify

--- app/helpers/usertop_helper 2.rb
+++
@@ -1,8 +0,0 @@
-module UsertopHelper
-     # 引数で与えられたユーザーのGravatar画像を返す
-    def gravatar_for(user, options = { size: 80 })
-        gravatar_id = Digest::MD5::hexdigest(user.password_digest)
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-        image_tag(gravatar_url, alt: user.name, class: "gravatar")
-    end
-end
