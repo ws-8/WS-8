c41ab4b2901a45a31b5cd6c25521b01b16ea7713
Ayato Watanabe <38004439+WhiteFox-Lugh@users.noreply.github.com>
2020-07-07 05:22:50 +0900

    fix :wrench: : recovery develop branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ApplicationRecord
   has_many :lessons_for_teacher,class_name:"Lesson",foreign_key:"user_id"
   has_many :questions
-  has_many:registrations
-  has_many:courses,through: :registrations
-  has_many:lessons,through: :registrations,through: :courses
+  has_many :registrations
+  has_many :courses, through: :registrations
+  has_many :lessons, through: :registrations
+  has_many :lessons, through: :courses
   #has_many:questions,through: :registrations,through: :courses,through: :lessons
 
-   has_secure_password
-   enum usertag:{
-   teacher: 0, #先生
-   ta: 1, #TA
-   student: 2, #生徒
-   admin: 3 #開発・保守専用
-   }
+  has_secure_password
+    enum usertag:{
+    teacher: 0, #先生
+    ta: 1, #TA
+    student: 2, #生徒
+    admin: 3 #開発・保守専用
+  }
 end
