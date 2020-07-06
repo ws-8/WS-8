94122a4d7ecac9d5523f55aee603fd007e0a3a51
Ayato Watanabe <38004439+WhiteFox-Lugh@users.noreply.github.com>
2020-07-07 06:20:16 +0900

    fix :wrench: : recover lecture menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def get_lectures
+    # get courses and lessons
+        @courses = Course.select("course_name")
+        query = 'SELECT course_name, title FROM lessons, courses WHERE courses.id == lessons.course_id'
+        @lessons = Course.find_by_sql(query)
+    end
 end
