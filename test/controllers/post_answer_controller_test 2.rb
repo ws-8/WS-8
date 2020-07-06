3ba3fa3cf78140d5dc8826a5f46c3ec816ae085a
Ayato Watanabe <38004439+WhiteFox-Lugh@users.noreply.github.com>
2020-07-07 08:18:27 +0900

    fix :wrench: : delete some files

--- test/controllers/post_answer_controller_test 2.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PostAnswerControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get post_answer_new_url
-    assert_response :success
-  end
-
-end
--- test/controllers/question_controller_test 2.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class QuestionControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get question_index_url
-    assert_response :success
-  end
-
-end
