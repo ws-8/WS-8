11526ba43674281b74c15c0054d72b5e69ed241d
nakanishi <nigiiro7716@gmail.com>
2020-07-12 11:38:06 +0900

    add post_question function

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,10 +1,35 @@
+# frozen_string_literal: true
+
 class QuestionController < ApplicationController
-  def index
+  def index; end
+
+  def new
+    @lesson = Lesson.find(params[:id])
+    @course = Course.find(@lesson.course_id)
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    binding.pry
+    if @question.save
+      redirect_to question_list_path,
+                  notice: 'success!'
+    else
+      flash[:alert] = 'Save error!'
+      redirect_to root_path
+    end
   end
 
   def list
-    @lesson=Lesson.find(params[:id])
-    @questions=(Question.where(lesson_id:params[:id])).page(params[:page]).per(10)
-    @course=Course.find(@lesson.course_id)
+    @lesson = Lesson.find(params[:id])
+    @questions = Question.where(lesson_id: params[:id]).page(params[:page]).per(10)
+    @course = Course.find(@lesson.course_id)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body, :user_id, :lesson_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'top#index'
 
@@ -9,9 +11,12 @@ Rails.application.routes.draw do
 
   get '/question_list/:id', to: 'question#list', as: :question_list
   get '/question_detail', to: 'question_detail#index'
+  get '/question/post_question/:id/new', to: 'question#new'
+  post '/question/post_question/:id', to: 'question#create'
 
   get '/post_answer', to: 'post_answer#new'
-  get '/post_question', to: 'post_question#new'
+  # get '/post_question', to: 'post_question#new'
+  # post '/post_question', to: 'post_question#create'
 
   get  '/signup', to: 'users#new'
 
@@ -24,5 +29,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :articles
   resources :users
+  # resources :question
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
