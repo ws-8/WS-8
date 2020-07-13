041dd137e5bba2f1273a60cee55b2f500d85c860
nakanishi <nigiiro7716@gmail.com>
2020-07-14 06:01:32 +0900

    fix: prevent the form from being blank when an error occurs

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,34 +1,39 @@
 # frozen_string_literal: true
 
 class QuestionController < ApplicationController
+  before_action :lesson_and_course
+
   def index; end
 
   def new
-    @lesson = Lesson.find(params[:id])
-    @course = Course.find(@lesson.course_id)
-    @question = Question.new
+    @question = Question.new(session[:question] || {})
   end
 
   def create
     @question = Question.new(question_params)
     # binding.pry
     if @question.save
+      session[:question] = nil
       redirect_to question_list_path, notice: 'Posted Successfully!'
     else
       flash[:alert] = @question.errors
+      session[:question] = @question.attributes.slice(*question_params.keys)
       redirect_to "/question/post_question/#{params[:id]}/new"
     end
   end
 
   def list
-    @lesson = Lesson.find(params[:id])
     @questions = Question.where(lesson_id: params[:id]).page(params[:page]).per(10)
-    @course = Course.find(@lesson.course_id)
   end
 
   private
 
   def question_params
     params.require(:question).permit(:title, :body, :user_id, :lesson_id)
+  end
+
+  def lesson_and_course
+    @lesson = Lesson.find(params[:id])
+    @course = Course.find(@lesson.course_id)
   end
 end
