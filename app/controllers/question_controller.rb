6b80cf7a4b180978e446c2eed660113b2f52ed49
Ayato Watanabe <38004439+WhiteFox-Lugh@users.noreply.github.com>
2020-07-14 10:58:37 +0900

    Revert "fix: prevent the form from being blank when an error occurs"
    This reverts commit 041dd137e5bba2f1273a60cee55b2f500d85c860.

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,39 +1,34 @@
 # frozen_string_literal: true
 
 class QuestionController < ApplicationController
-  before_action :lesson_and_course
-
   def index; end
 
   def new
-    @question = Question.new(session[:question] || {})
+    @lesson = Lesson.find(params[:id])
+    @course = Course.find(@lesson.course_id)
+    @question = Question.new
   end
 
   def create
     @question = Question.new(question_params)
     # binding.pry
     if @question.save
-      session[:question] = nil
       redirect_to question_list_path, notice: 'Posted Successfully!'
     else
       flash[:alert] = @question.errors
-      session[:question] = @question.attributes.slice(*question_params.keys)
       redirect_to "/question/post_question/#{params[:id]}/new"
     end
   end
 
   def list
+    @lesson = Lesson.find(params[:id])
     @questions = Question.where(lesson_id: params[:id]).page(params[:page]).per(10)
+    @course = Course.find(@lesson.course_id)
   end
 
   private
 
   def question_params
     params.require(:question).permit(:title, :body, :user_id, :lesson_id)
-  end
-
-  def lesson_and_course
-    @lesson = Lesson.find(params[:id])
-    @course = Course.find(@lesson.course_id)
   end
 end
