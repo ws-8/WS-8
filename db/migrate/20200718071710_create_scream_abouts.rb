67ed4118a35fd9c33531da5efd5b7b0024378236
yamakai <restartsugar@gmail.com>
2020-07-18 16:33:56 +0900

    CreateScreamAboutをnil:falseに変更

--- db/migrate/20200718071710_create_scream_abouts.rb
+++ db/migrate/20200718071710_create_scream_abouts.rb
@@ -1,7 +1,7 @@
 class CreateScreamAbouts < ActiveRecord::Migration[5.1]
   def change
     create_table :scream_abouts do |t|
-      t.integer rided_score, null: false, default: 0
+      t.integer rided_score, nil: false, default: 0
       t.references :scream, foreign_key: true, nil: false
       t.references :question, foreign_key: true, nil: false
 
