6bcc64b5baa919ec71483a6780ff19ebc8faa844
yamakai <restartsugar@gmail.com>
2020-07-18 16:29:02 +0900

    references込みでscream_idとquestion_idを追加

---
+++ db/migrate/20200718071710_create_scream_abouts.rb
@@ -0,0 +1,11 @@
+class CreateScreamAbouts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :scream_abouts do |t|
+      t.integer rided_score, null: false, default: 0
+      t.references :scream, foreign_key: true, nil: false
+      t.references :question, foreign_key: true, nil: false
+
+      t.timestamps
+    end
+  end
+end
