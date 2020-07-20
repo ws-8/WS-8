0e4a5016b26f9af4c9856c1e7e9d16d4c6caf62f
Ayato Watanabe <38004439+WhiteFox-Lugh@users.noreply.github.com>
2020-07-20 19:13:20 +0900

    feat :new: : syntaxhighlight

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,27 +1,35 @@
+require 'rouge/plugins/redcarpet'
+
+class HTML < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+end
+
 module MarkdownHelper
   def markdown(text)
+    options = {
+      filter_html: true,
+      hard_wrap: true
+    }
+    extensions = {
+      no_intra_emphasis: true,
+      tables: true,
+      fenced_code_blocks: true,
+      autolink: true,
+      quote: true,
+      space_after_headers: true,
+      with_toc_data: true,
+      xhtml: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      tables: true,
+      no_intra_emphasis: true
+    }
+
     unless @markdown
-      options = {
-        filter_html: true,
-        hard_wrap: true,
-        space_after_headers: true,
-        with_toc_data: true,
-        xhtml: true,
-        lax_html_blocks: true,
-        strikethrough: true,
-        tables: true,
-        no_intra_emphasis: true
-      }
-      extensions = {
-        no_intra_emphasis: true,
-        tables: true,
-        fenced_code_blocks: true,
-        autolink: true,
-        quote: true
-      }
-      renderer = Redcarpet::Render::HTML.new(options)
+      renderer = HTML.new(options)
       @markdown = Redcarpet::Markdown.new(renderer, extensions)
     end
+
     @markdown.render(text).html_safe
   end
 end
